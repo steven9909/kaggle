d8f8da6d38d75172aebbd9a8b3170c699e70913a
Benny Huang <bennyhuang@Bennys-MacBook-Pro.local>
2022-09-05 03:37:06 -0400

    initial commit

---
+++ google-universal-image-embedder/README.md
@@ -0,0 +1,12 @@
+# Google Universal Image Embedding
+
+https://www.kaggle.com/competitions/google-universal-image-embedding
+
+# Requirements
+
+```
+kaggle==1.5.12
+pytorch-lightning==1.7.4
+pytube==12.1.0
+transformers==4.21.2
+```
---
+++ google-universal-image-embedder/dataset/utils.py
@@ -0,0 +1,15 @@
+from pytube import Search
+
+keywords = [
+    "apparel & accessories",
+    "packaged goods",
+    "landmarks",
+    "furniture & home decor",
+    "storefronts",
+    "dishes",
+    "artwork",
+    "toys",
+    "memes",
+    "illustrations",
+    "cars",
+]
