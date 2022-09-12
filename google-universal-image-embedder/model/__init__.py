622389367d1e913508ca6e2ade32999ae25ed406
Benny Huang <bennyhuang@Bennys-MacBook-Pro.local>
2022-09-12 15:37:17 -0400

    refactor and not working

--- google-universal-image-embedder/model/__init__.py
+++ google-universal-image-embedder/model/__init__.py
@@ -1,57 +1,53 @@
+from typing import List
+
 import numpy as np
 import pytorch_lightning as pl
 import torch.nn as nn
-import torchvision.transforms.functional as TF
-from dataset import VideoDataModule
-from torch import cat, from_numpy, optim, roll
+from torch import BoolTensor, Tensor, cat, optim, roll
 
 from model.transformer.model import ViT
 
 
-IMAGE_SIZE = 224
-D_TOKEN = 64
-N_ENC = 4
-N_DEC = 4
-HEADS = 8
-FRAME_COUNT = 16
-PATCH_SIZE = 16
-SEQ_LEN = ((IMAGE_SIZE // PATCH_SIZE) ** 2) * FRAME_COUNT
+def construct_mask(seq_len: int) -> BoolTensor:
 
-
-def transform(x: np.ndarray):
-    y = TF.to_tensor(x)
-    y = TF.resize(y, [IMAGE_SIZE, IMAGE_SIZE])
-    y = TF.normalize(y, [0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-    return y
-
-
-def get_mask(seq_len: int):
-    mask = np.tril(np.ones((1, seq_len, seq_len))).astype("uint8")
-    mask = from_numpy(mask)
-    return mask
+    return BoolTensor(np.tril(np.ones((1, seq_len, seq_len))))
 
 
 class Model(pl.LightningModule):
+    """
+    Model
+
+    Args:
+        image_size (int): Image size
+        patch_size (int): Patch size
+        in_channels (int): Number of input channels
+        n_heads (int): Number of attention heads
+        d_token (int): Dimension of each token
+        n_enc_layers (int): Number of encoder layers
+        n_dec_layers (int): Number of decoder layers
+        seq_len (int): Sequence length
+    """
+
     def __init__(
         self,
-        d_token,
-        n_encoder,
-        n_decoder,
-        heads,
-        seq_len,
-        patch_size,
-        in_channels,
-        image_size,
+        image_size: int,
+        patch_size: int,
+        in_channels: int,
+        n_heads: int,
+        d_token: int,
+        n_enc_layers: int,
+        n_dec_layers: int,
+        seq_len: int,
     ):
-        super().__init__()
-        mask = get_mask(seq_len)
-        self.register_buffer("mask", mask)
 
+        super().__init__()
+        mask = construct_mask(seq_len // n_heads)
+        self.register_buffer("mask", mask)
         self.model = ViT(
             d_token=d_token,
-            heads=heads,
-            n_encoder=n_encoder,
-            n_decoder=n_decoder,
+            heads=n_heads,
+            n_encoder=n_enc_layers,
+            n_decoder=n_dec_layers,
             seq_len=seq_len,
             patch_size=patch_size,
             in_channels=in_channels,
@@ -59,33 +55,55 @@ class Model(pl.LightningModule):
         )
         self.loss = nn.MSELoss()
 
-    def forward(self, x, mask):
+    def forward(self, x: Tensor, mask: BoolTensor) -> Tensor:
+        """
+        Forward pass of Model
+
+        Note:
+            N: batch size
+            S: sequence length
+            C: channel size
+            H: image height
+            W: image width
+
+        Args:
+            x (Tensor): Input tensor of shape (N, C, S * H, W)
+            mask (BoolTensor): Input mask
+
+        Returns:
+            Tensor: Output of shape (N, C, S * H, W)
+        """
+
         return self.model(x, mask)
 
     def configure_optimizers(self):
+        """
+        Configure optimizers
+        """
+
         return optim.Adam(self.parameters(), lr=0.002)
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch: List[Tensor], _):
+        """
+        Training step
+
+        Note:
+            N: batch size
+            S: sequence length
+            C: channel size
+            H: image height
+            W: image width
+
+        Args:
+            batch (List[Tensor]): list of S tensors of shape (N, C, H, W)
+        """
+
+        # TODO: needs rework
         current_batch = cat(batch, dim=2)
         target_batch = roll(current_batch, -1)
 
         output = self(current_batch, self.mask)
         output[:, :, -1, -1] = target_batch[:, :, -1, -1]
+        # TODO: needs rework
 
         return self.loss(output, target_batch)
-
-
-if __name__ == "__main__":
-    datamodule = VideoDataModule("./videos/", 16, transform=transform, batch_size=2)
-    model = Model(
-        d_token=D_TOKEN,
-        heads=HEADS,
-        n_encoder=N_ENC,
-        n_decoder=N_DEC,
-        seq_len=SEQ_LEN,
-        patch_size=PATCH_SIZE,
-        in_channels=3,
-        image_size=IMAGE_SIZE,
-    )
-    trainer = pl.Trainer(max_epochs=2, accelerator="gpu")
-    trainer.fit(model, datamodule=datamodule)
---
+++ google-universal-image-embedder/train.py
@@ -0,0 +1,35 @@
+import hydra
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+
+from dataset import VideoDataModule
+from model import Model
+from torchvision import transforms as T
+
+
+@hydra.main(".", "config.yaml", None)
+def main(config: DictConfig):
+    transform = T.Compose(
+        [
+            T.ToTensor(),
+            T.Resize((config.image_size, config.image_size)),
+            T.Normalize(config.normalize.mean, config.normalize.std),
+        ]
+    )
+    data_module = VideoDataModule("data/", config.seq_len, config.batch_size, transform)
+    model = Model(
+        config.image_size,
+        config.patch_size,
+        config.in_channels,
+        config.n_heads,
+        config.d_token,
+        config.n_enc_layers,
+        config.n_dec_layers,
+        config.seq_len,
+    )
+    trainer = pl.Trainer(max_epochs=2, accelerator="cpu")
+    trainer.fit(model, data_module)
+
+
+if __name__ == "__main__":
+    main()
