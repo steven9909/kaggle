0e4064bd49d1047d61c90f93a81ad0b3a88ea7d8
steven9909 <sshyun@edu.uwaterloo.ca>
2022-09-13 23:03:23 -0400

    add checkpoint

--- google-universal-image-embedder/train.py
+++ google-universal-image-embedder/train.py
@@ -1,3 +1,4 @@
+from genericpath import isdir
 import hydra
 import pytorch_lightning as pl
 from omegaconf import DictConfig
@@ -5,6 +6,9 @@ from omegaconf import DictConfig
 from dataset import VideoDataModule
 from model import Model
 from torchvision import transforms as T
+
+import os
+from pathlib import Path
 
 
 @hydra.main(".", "config.yaml", None)
@@ -23,6 +27,22 @@ def main(config: DictConfig):
     image_size = config.image_size
     patch_size = config.patch_size
 
+    def get_latest_checkpoint(checkpoint_dir: Path):
+        max_version_num = -1
+        checkpoint_dir = checkpoint_dir / "lightning_logs"
+        for file in checkpoint_dir.iterdir():
+            print(file)
+            if file.is_dir():
+                max_version_num = max(max_version_num, int(file.stem.split("_")[-1]))
+
+        if max_version_num == -1:
+            return None
+
+        checkpoint_dir = checkpoint_dir / f"version_{max_version_num}/checkpoints/"
+        for file in checkpoint_dir.iterdir():
+            if file.suffix == ".ckpt":
+                return file
+
     model = Model(
         image_size,
         patch_size,
@@ -35,13 +55,14 @@ def main(config: DictConfig):
         config.clip_len,
     )
     trainer = pl.Trainer(
-        default_root_dir=config.checkpoint_dir, max_epochs=100, accelerator="gpu"
+        default_root_dir=config.checkpoint_dir,
+        max_epochs=100,
+        accelerator="gpu",
+        resume_from_checkpoint=get_latest_checkpoint(Path(config.checkpoint_dir))
+        if config.use_checkpoints
+        else None,
     )
-    trainer.fit(
-        model,
-        data_module,
-        ckpt_path=config.checkpoint_dir if config.use_checkpoints else None,
-    )
+    trainer.fit(model, data_module)
 
 
 if __name__ == "__main__":
