b5bb08a2827c182ecc8c57f44b87794d3b7ec562
Benny Huang <bennyhuang@Bennys-MacBook-Pro.local>
2022-09-18 13:24:25 -0400

    refactor

--- google-universal-image-embedder/model/model.py
+++ google-universal-image-embedder/model/model.py
@@ -6,11 +6,16 @@ from torch import Tensor, nn, no_grad, optim
 from torchvision import models
 
 
-class EncoderBlock(nn.Module):
+class EncoderStack(nn.Module):
     def __init__(self, in_features: int, out_features: int, n: int):
 
+        assert n > 0, f"n should be greater than zero, but got {n}"
+
         super().__init__()
-        self.linears = nn.Sequential(
+        self.model = nn.Sequential(
+            nn.Linear(in_features, out_features),
+            nn.BatchNorm1d(out_features),
+            nn.GELU(),
             *[
                 nn.Sequential(
                     nn.Linear(in_features, in_features),
@@ -18,22 +23,22 @@ class EncoderBlock(nn.Module):
                     nn.GELU(),
                 )
                 for _ in range(n - 1)
-            ]
+            ],
         )
-        self.last = nn.Linear(in_features, out_features)
-        self.last_norm = nn.BatchNorm1d(out_features)
 
     def forward(self, x: Tensor) -> Tensor:
 
-        return self.last_norm(self.last(self.linears(x)))
+        return self.model(x)
 
 
-class DecoderBlock(nn.Module):
+class DecoderStack(nn.Module):
     def __init__(self, in_features: int, out_features: int, n: int):
 
+        assert n > 0, f"n should be greater than zero, but got {n}"
+
         super().__init__()
-        self.first = nn.Linear(in_features, out_features)
-        self.linears = nn.Sequential(
+        self.model = nn.Sequential(
+            nn.Linear(in_features, out_features),
             *[
                 nn.Sequential(
                     nn.BatchNorm1d(out_features),
@@ -41,39 +46,29 @@ class DecoderBlock(nn.Module):
                     nn.Linear(out_features, out_features),
                 )
                 for _ in range(n - 1)
-            ]
+            ],
         )
 
     def forward(self, x: Tensor) -> Tensor:
 
-        return self.linears(self.first(x))
+        return self.model(x)
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self, encoder_size: int = 2, decoder_size: int = 2):
+    def __init__(self, encoder_size: int = 1, decoder_size: int = 1):
 
         super().__init__()
         self.encoder = nn.Sequential(
-            EncoderBlock(1024, 512, encoder_size),
-            nn.GELU(),
-            EncoderBlock(512, 256, encoder_size),
-            nn.GELU(),
-            EncoderBlock(256, 128, encoder_size),
-            nn.GELU(),
-            EncoderBlock(128, 64, encoder_size),
-            nn.GELU(),
+            EncoderStack(1024, 512, encoder_size),
+            EncoderStack(512, 256, encoder_size),
+            EncoderStack(256, 128, encoder_size),
+            EncoderStack(128, 64, encoder_size),
         )
         self.decoder = nn.Sequential(
-            DecoderBlock(64, 128, decoder_size),
-            nn.BatchNorm1d(128),
-            nn.GELU(),
-            DecoderBlock(128, 256, decoder_size),
-            nn.BatchNorm1d(256),
-            nn.GELU(),
-            DecoderBlock(256, 512, decoder_size),
-            nn.BatchNorm1d(512),
-            nn.GELU(),
-            DecoderBlock(512, 1024, decoder_size),
+            DecoderStack(64, 128, decoder_size),
+            DecoderStack(128, 256, decoder_size),
+            DecoderStack(256, 512, decoder_size),
+            DecoderStack(512, 1024, decoder_size),
         )
 
     def forward(self, x: Tensor) -> Tensor:
@@ -99,10 +94,16 @@ class Model(pl.LightningModule):
         return y, y_hat
 
     def training_step(self, batch: Tensor, batch_idx: int) -> Tensor:
-        loss = F.mse_loss(*self.forward(batch))
-        self.log("loss", loss)
+
+        y, y_hat = self.forward(batch)
+        loss = F.mse_loss(y_hat, y)
+
         return loss
 
     def configure_optimizers(self) -> optim.Optimizer:
 
         return optim.Adam(self.parameters(), lr=0.0005)
+
+
+if __name__ == "__main__":
+    print(AutoEncoder(2, 2))
