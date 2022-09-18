3b88fa314eee1f97ff9af3961bddad25ddcce9c7
steven9909 <sshyun@edu.uwaterloo.ca>
2022-09-18 14:07:36 -0400

    update process

--- google-universal-image-embedder/dataset/pretrain_process.py
+++ google-universal-image-embedder/dataset/pretrain_process.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Lock, RLock
 from pathlib import Path
+from threading import local
 from torchvision.models import VisionTransformer
 from typing import Iterator, Optional, Callable
 from torch import Tensor
@@ -10,6 +11,7 @@ from dataset.dataset import Dataset
 import string
 import random
 from datetime import datetime
+from copy import deepcopy
 
 
 class PretrainLoader:
@@ -27,14 +29,25 @@ class PretrainLoader:
         self.num_threads = num_threads
         random.seed(datetime.now())
 
-    def _process_image(self, image: Tensor):
-        image = self.model.forward(image)
+    def _process_image(self, image: Tensor, local):
+        image = local.model.forward(image)
         torch.save(image, self.save_dir / self.generate_filename())
 
     def process(self):
-        executor = ThreadPoolExecutor(max_workers=self.num_threads)
+        def init_worker(local):
+            model = deepcopy(self.model)
+            model.eval()
+            local.model = model
+
+        thread_local = local()
+        executor = ThreadPoolExecutor(
+            initializer=init_worker,
+            init_args=(thread_local),
+            max_workers=self.num_threads,
+        )
+
         for image in self.dataset.train_dataloader():
-            executor.submit(self._process_image, image)
+            executor.submit(self._process_image, image, thread_local)
 
     def generate_filename(self, N=30) -> str:
         return "".join(random.choices(string.ascii_uppercase + string.digits, k=30))
