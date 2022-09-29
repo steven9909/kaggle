b88791e15d90bef60f189c6db543511901fd28af
Benny Huang <bennyhuang@Bennys-MacBook-Pro.local>
2022-09-29 16:46:18 -0400

    vicreg

--- google-universal-image-embedder/model/ssl_framework.py
+++ google-universal-image-embedder/model/ssl_framework.py
@@ -6,12 +6,12 @@ import pytorch_lightning as pl
 
 
 class SimCLRLoss(nn.Module):
-    def __init__(self, batch_size: int, temperature: float):
+    def __init__(self, batch_size: int, simclr_temperature: float):
 
         super().__init__()
 
         self.batch_size = batch_size
-        self.temperature = temperature
+        self.simclr_temperature = simclr_temperature
 
     def _cosine_similarity_matrix(self, z: Tensor) -> Tensor:
 
@@ -22,8 +22,8 @@ class SimCLRLoss(nn.Module):
         z1 = F.normalize(z1, dim=1)
         z2 = F.normalize(z2, dim=1)
 
-        z = torch.cat([z1, z2])
-        exp = torch.exp(self._cosine_similarity_matrix(z) / self.temperature)
+        sim = self._cosine_similarity_matrix(torch.cat([z1, z2], 0))
+        exp = torch.exp(sim / self.simclr_temperature)
 
         mask = torch.eye(2 * self.batch_size).bool()
         pmask = mask.roll(self.batch_size, 0)
@@ -34,55 +34,91 @@ class SimCLRLoss(nn.Module):
         )
 
 
-class ContrastiveLoss(nn.Module):
-    """
-    Using this for benchmark (DELETE LATER)
-    https://zablo.net/blog/post/understanding-implementing-simclr-guide-eli5-pytorch/
-    """
+class VICRegLoss(nn.Module):
+    def __init__(
+        self,
+        batch_size: int,
+        repr_size: int,
+        vicreg_lambda: float,
+        vicreg_mu: float,
+        vicreg_nu: float,
+    ):
 
-    def __init__(self, batch_size, temperature=0.5):
         super().__init__()
+
         self.batch_size = batch_size
-        self.register_buffer("temperature", torch.tensor(temperature))
-        self.register_buffer(
-            "negatives_mask",
-            (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool)).float(),
-        )
+        self.repr_size = repr_size
+        self.vicreg_lambda = vicreg_lambda
+        self.vicreg_mu = vicreg_mu
+        self.vicreg_nu = vicreg_nu
 
-    def forward(self, emb_i, emb_j):
-        """
-        emb_i and emb_j are batches of embeddings, where corresponding indices are pairs
-        z_i, z_j as per SimCLR paper
-        """
-        z_i = F.normalize(emb_i, dim=1)
-        z_j = F.normalize(emb_j, dim=1)
+    def _invariance(self, z1: Tensor, z2: Tensor) -> Tensor:
 
-        representations = torch.cat([z_i, z_j], dim=0)
-        similarity_matrix = F.cosine_similarity(
-            representations.unsqueeze(1), representations.unsqueeze(0), dim=2
-        )
+        return F.mse_loss(z1, z2)
 
-        sim_ij = torch.diag(similarity_matrix, self.batch_size)
-        sim_ji = torch.diag(similarity_matrix, -self.batch_size)
-        positives = torch.cat([sim_ij, sim_ji], dim=0)
+    def _variance(self, z1: Tensor, z2: Tensor) -> Tensor:
 
-        nominator = torch.exp(positives / self.temperature)
-        denominator = self.negatives_mask * torch.exp(
-            similarity_matrix / self.temperature
-        )
+        std_z1 = torch.sqrt(torch.var(z1, 0) + 1e-4)
+        std_z2 = torch.sqrt(torch.var(z2, 0) + 1e-4)
 
-        loss_partial = -torch.log(nominator / torch.sum(denominator, dim=1))
-        loss = torch.sum(loss_partial) / (2 * self.batch_size)
-        return loss
+        return (torch.mean(F.relu(1 - std_z1)) + torch.mean(F.relu(1 - std_z2))) / 2
+
+    def _covariance(self, z1: Tensor, z2: Tensor) -> Tensor:
+
+        z1 = z1 - torch.mean(z1, 0)
+        z2 = z2 - torch.mean(z2, 0)
+
+        cov_z1 = (z1.T @ z1) / (self.batch_size - 1)
+        cov_z2 = (z2.T @ z2) / (self.batch_size - 1)
+
+        mask = ~torch.eye(self.repr_size).bool()
+
+        return (
+            torch.sum(torch.pow(mask * cov_z1, 2))
+            + torch.sum(torch.pow(mask * cov_z2, 2))
+        ) / self.repr_size
+
+    def forward(self, z1: Tensor, z2: Tensor) -> Tensor:
+
+        inv = self._invariance(z1, z2)
+        var = self._variance(z1, z2)
+        cov = self._covariance(z1, z2)
+
+        return self.vicreg_lambda * inv + self.vicreg_mu * var + self.vicreg_nu * cov
+
+
+def off_diagonal(x):
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+
+def forward(x, y, batch_size, repr_size, sim_coeff, std_coeff, cov_coeff):
+    repr_loss = F.mse_loss(x, y)
+
+    x = x - x.mean(dim=0)
+    y = y - y.mean(dim=0)
+
+    std_x = torch.sqrt(x.var(dim=0) + 0.0001)
+    std_y = torch.sqrt(y.var(dim=0) + 0.0001)
+    std_loss = torch.mean(F.relu(1 - std_x)) / 2 + torch.mean(F.relu(1 - std_y)) / 2
+
+    cov_x = (x.T @ x) / (batch_size - 1)
+    cov_y = (y.T @ y) / (batch_size - 1)
+    cov_loss = 0
+    cov_loss += off_diagonal(cov_x).pow_(2).sum().div(repr_size)
+    cov_loss += off_diagonal(cov_y).pow_(2).sum().div(repr_size)
+
+    return sim_coeff * repr_loss + std_coeff * std_loss + cov_coeff * cov_loss
 
 
 if __name__ == "__main__":
-    from time import time_ns
-
-    l1 = SimCLRLoss(16, 0.5)
-    l2 = ContrastiveLoss(16, 0.5)
+    l1 = VICRegLoss(16, 1024, 25, 25, 1)
 
     z1 = torch.randn(16, 1024)
     z2 = torch.randn(16, 1024)
 
-    assert l1(z1, z2) == l2(z1, z2)
+    print(l1(z1, z2))
+    print(forward(z1, z2, 16, 1024, 25, 25, 1))
+
+    assert l1(z1, z2) == forward(z1, z2, 16, 1024, 25, 25, 1)
