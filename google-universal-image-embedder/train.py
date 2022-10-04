d5e908c8a95a86cea2a65143a3792ae6c9f1834c
steven9909 <sshyun@edu.uwaterloo.ca>
2022-10-03 23:31:03 -0400

    byol

--- google-universal-image-embedder/train.py
+++ google-universal-image-embedder/train.py
@@ -25,38 +25,32 @@ def main(config: DictConfig):
         kaggles, [Extension.JPEG, Extension.JPG], batch_size=16, num_workers=10
     )
 
-    data_module.setup("")
+    ckpt_path = Path(config.checkpoint_dir)
+    checkpoint = ModelCheckpoint(
+        dirpath=ckpt_path,
+        save_top_k=-1,
+        monitor="loss",
+        save_last=True,
+    )
+    ckpt_path = ckpt_path / "last.ckpt" if config.load_from_checkpoint else None
 
-    for data in data_module.train_dataloader():
-        print(data)
-        break
+    repr_model = convnext_base(weights=ConvNeXt_Base_Weights.DEFAULT)
 
-    # ckpt_path = Path(config.checkpoint_dir)
-    # checkpoint = ModelCheckpoint(
-    #     dirpath=ckpt_path,
-    #     save_top_k=-1,
-    #     monitor="loss",
-    #     save_last=True,
-    # )
-    # ckpt_path = ckpt_path / "last.ckpt" if config.load_from_checkpoint else None
+    for name, param in repr_model.named_parameters():
+        if not (
+            name.startswith("features.6")
+            or name.startswith("features.7")
+            or name.startswith("classifier")
+        ):
+            param.requires_grad = False
 
-    # repr_model = convnext_base(weights=ConvNeXt_Base_Weights.DEFAULT)
+    repr_model.classifier[2] = Identity()
 
-    # for name, param in repr_model.named_parameters():
-    #     if not (
-    #         name.startswith("features.6")
-    #         or name.startswith("features.7")
-    #         or name.startswith("classifier")
-    #     ):
-    #         param.requires_grad = False
-
-    # repr_model.classifier[2] = Identity()
-
-    # model = BYOLLightningModule(repr_model, 1024)
-    # trainer = pl.Trainer(
-    #     max_epochs=500, accelerator="gpu", devices=1, callbacks=[checkpoint]
-    # )
-    # trainer.fit(model, data_module, ckpt_path=ckpt_path)
+    model = BYOLLightningModule(repr_model, 1024)
+    trainer = pl.Trainer(
+        max_epochs=500, accelerator="gpu", devices=1, callbacks=[checkpoint]
+    )
+    trainer.fit(model, data_module, ckpt_path=ckpt_path)
 
 
 if __name__ == "__main__":
